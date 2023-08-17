208c59593c0ac7948064831c8d544071b1016607
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2023-08-17 11:31:09 -0300

    unidade 1

--- unidade1/classification_decision_tree/1_decision_tree.py
+++ unidade1/classification_decision_tree/1_decision_tree.py
@@ -1,10 +1,11 @@
-# Import DecisionTreeClassifier from sklearn.tree
-from sklearn.tree import DecisionTreeClassifier
 from src.utils import load_breast_cancer_dataset
 from sklearn.model_selection import train_test_split
 
+# Import DecisionTreeClassifier from sklearn.tree
+_____
+
 df_breast = load_breast_cancer_dataset()
-X = df_breast[["diagnosis", "radius_mean","texture_mean","perimeter_mean","area_mean"]].values
+X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean"]].values
 y  = df_breast[['diagnosis']].values
 
 # Instantiate a DecisionTreeClassifier 'dt' with a maximum depth of 6
@@ -17,7 +18,12 @@ X_train, X_test, y_train, y_test =___
 ___
 
 # Predict test set labels
-___
+y_pred =
 
 #print predicted labels
 __
+
+# Compute test set accuracy
+acc = ___
+#print the score
+print("Test set accuracy: {:.2f}".format(acc))
