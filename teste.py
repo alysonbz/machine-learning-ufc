6fa36b17da12544e4e67a143bad440ab9824360e
Thays Ferreira <Thayzsferreiraua@gmail.com>
2023-08-24 15:41:35 -0300

    t

---
+++ teste.py
@@ -0,0 +1,13 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+x_train, x_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size = 0.2,
+                                                    stractfy = )
+
+df.fit(x_train, y_train)
+
+y_pred = df.predict(x_test)
+
+accuracy_score(y_test, y_pred)
