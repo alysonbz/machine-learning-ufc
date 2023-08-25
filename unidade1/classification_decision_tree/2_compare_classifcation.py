4af5ce84efb88cb0899c774f700b4e350c79e4c3
Laura Mendes <104784772+lauramendescd@users.noreply.github.com>
2023-08-25 12:32:39 -0300

    Update 2_compare_classifcation.py
    Atualização

--- unidade1/classification_decision_tree/2_compare_classifcation.py
+++ unidade1/classification_decision_tree/2_compare_classifcation.py
@@ -1,29 +1,36 @@
 # Import LogisticRegression from sklearn.linear_model
 from src.utils import load_breast_cancer_dataset
-from sklearn.linear_model import  LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 
-def process_classifier(clf,X_train, X_test, y_train, y_test):
-    ---
-
-
+def process_classifier(clf, X_train, X_test, y_train, y_test):
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(acc)
 
 df_breast = load_breast_cancer_dataset()
-X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean","smoothness_mean"]].values
-y  = df_breast[['diagnosis']].values
+
+X = df_breast.values
+y = df_breast[['diagnosis']].values
+
 
 # Instatiate logreg and decision tree
-logreg = ----
-dt = --
+logreg = LogisticRegression()
+dt = DecisionTreeClassifier()
+
 
 # split the data
-X_train, X_test, y_train, y_test = ---
-# Fit logreg to the training set
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    stratify=y,
+                                                    random_state=1)
 
 # call function to process log_reg
---
+process_classifier(logreg, X_train, X_test, y_train, y_test)
+
 # call function to process dt
-__
+process_classifier(dt, X_train, X_test, y_train, y_test)
