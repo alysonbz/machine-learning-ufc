eb3b2d0092052c0e6927dd700e50fdd9adf25583
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2023-09-14 14:38:16 -0300

    unidade 1

---
+++ unidade1/classification_decision_tree/7_calculos_decison_tree_regression.py
@@ -0,0 +1,12 @@
+
+from src.utils import load_house_price_dataset
+
+df_house = load_house_price_dataset()
+
+
+def questao1():
+    return None
+def questao2():
+    return None
+def questao3():
+    return None
