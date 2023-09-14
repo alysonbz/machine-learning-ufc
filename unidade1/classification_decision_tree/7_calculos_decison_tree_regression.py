aa0c667961b2e5fed09852eecaa206912fc5216a
lauramendescd <lauramendescd@gmail.com>
2023-09-14 16:57:34 -0300

    atualização

--- unidade1/classification_decision_tree/7_calculos_decison_tree_regression.py
+++ unidade1/classification_decision_tree/7_calculos_decison_tree_regression.py
@@ -1,12 +1,25 @@
-
+import numpy
+import pandas
 from src.utils import load_house_price_dataset
-
 df_house = load_house_price_dataset()
 
 
 def questao1():
-    return None
+    count = df_house['price'].count()
+    avg = df_house['price'].mean()
+    std = df_house['price'].std()
+    cv = std/avg * 100
+    return count, avg, std, cv
+
+count, avg, std, cv = questao1()
+print(count)
+print(avg)
+print(std)
+print(cv)
 def questao2():
+
+
+
     return None
 def questao3():
     return None
@@ -14,3 +27,6 @@ def questao3():
 print("Questao1: ",questao1())
 print("Questao2: ",questao2())
 print("Questao3: ",questao3())
+
+
+
