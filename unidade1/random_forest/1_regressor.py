ca28e3790f37c07ca18db12ddbd1145ac3d430a0
Thays Ferreira <Thayzsferreiraua@gmail.com>
2023-10-05 16:24:29 -0300

    exercicios

--- unidade1/random_forest/1_regressor.py
+++ unidade1/random_forest/1_regressor.py
@@ -1,46 +1,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 # Import RandomForestRegressor
-____
+from sklearn.ensemble import RandomForestRegressor
 
 # Import mean_squared_error as MSE
-from .____ import ____ as ____
+from sklearn.metrics import mean_squared_error as MSE
 
 from src.utils import bike_rental_dataset
 from sklearn.model_selection import train_test_split
 
 df = bike_rental_dataset()
-X = df.drop(['count'],axis = 1)
+X = df.drop(['count'], axis=1)
 y = df['count'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=2)
 
 
 # Instantiate rf
-rf = ____(n_estimators=____,
-          random_state=2)
+rf = RandomForestRegressor(n_estimators=100,
+                           random_state=2)
 
 # Fit rf to the training set
-____.____(____, ____)
+rf.fit(X_train, y_train)
 
 
 # Predict the test set labels
-y_pred = ____.____(____)
+y_pred = rf.predict(X_test)
 
 # Evaluate the test set RMSE
-rmse_test = ____
+rmse_test = MSE(y_test, y_pred) ** 0.5
+
 
 # Print rmse_test
 print('Test set RMSE of rf: {:.2f}'.format(rmse_test))
 
 
 # Create a pd.Series of features importances
-importances = ___
+importances = pd.Series(rf.feature_importances_, index=X.columns)
 
 # Sort importances
-importances_sorted =___
+importances_sorted = importances.sort_values(ascending=False)
 
 # Draw a horizontal barplot of importances_sorted
-____
+importances_sorted.plot(kind='barh')
 plt.title('Features Importances')
 plt.show()
