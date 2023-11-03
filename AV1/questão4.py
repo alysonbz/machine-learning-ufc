96b2bc88e293de8d86706ce11994889bc779bf6b
Thays Ferreira <Thayzsferreiraua@gmail.com>
2023-11-03 20:08:19 -0300

    exercicios

--- AV1/questão4.py
+++ AV1/questão4.py
@@ -58,16 +58,3 @@ results.sort(key=lambda x: x[1], reverse=True)
 print("Comparação de Desempenho dos Classificadores:")
 for clf_name, accuracy in results:
     print(f'{clf_name}: {accuracy:.3f}')
-
-# Instantiate a VotingClassifier vc
-vc = VotingClassifier(estimators=classifiers)
-
-# Fit vc to the training set
-vc.fit(X_train, y_train)
-
-# Evaluate the test set predictions
-y_pred = vc.predict(X_test)
-
-# Calculate accuracy score
-accuracy = accuracy_score(y_test, y_pred)
-print('Voting Classifier: {:.3f}'.format(accuracy))
