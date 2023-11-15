46402f70468445e67349e6b298d208647d89d6e6
Bruna Barreto <bbmbarreto@alu.ufc.br>
2023-11-15 15:19:08 -0300

    Commit de algumas mudanças 2

--- unidade1/4_boost/2_gradient_boost.py
+++ unidade1/4_boost/2_gradient_boost.py
@@ -1,31 +1,31 @@
 from sklearn.model_selection import train_test_split
 # Import GradientBoostingRegressor
-_____
+from sklearn.ensemble import GradientBoostingClassifier
 # Import mean_squared_error as MSE
 from sklearn.metrics import mean_squared_error as MSE
 from src.utils import bike_rental_dataset
 
+SEED = 2
 df = bike_rental_dataset()
 X = df.drop(['count'],axis = 1)
 y = df['count'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=2)
-
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=SEED)
 
 # Instantiate gb
-gb = ___
+gb = GradientBoostingClassifier(n_estimators=20,max_depth=1,random_state=2)
 
-# Fit gb to the training set
-___
+# Fit gb to the training set00,
+gb.fit(X_train,y_train)
 
 # Predict test set labels
-y_pred = __
+y_pred = gb.predict_proba(X_test)
 
 # Compute MSE
-mse_test = __
+mse_test = MSE(y_test,y_pred)
 
 # Compute RMSE
-rmse_test = ___
+rmse_test = mse_test**(1/2)
 
 # Print RMSE
-print('Test set RMSE of gb: {:.3f}'.format(___))
+print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
