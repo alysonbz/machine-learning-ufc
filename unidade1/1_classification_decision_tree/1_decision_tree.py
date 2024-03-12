5a4c87ea8a6a4eec1ca0d9554cd0a122a80ed8f4
Sheldaa <sheldasouza@alu.ufc.br>
2024-03-12 15:53:56 -0300

    primeira aula :)

--- unidade1/1_classification_decision_tree/1_decision_tree.py
+++ unidade1/1_classification_decision_tree/1_decision_tree.py
@@ -1,29 +1,30 @@
 from src.utils import load_breast_cancer_dataset
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 # Import DecisionTreeClassifier from sklearn.tree
-_____
+from sklearn.tree import DecisionTreeClassifier
 
 df_breast = load_breast_cancer_dataset()
 X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean"]].values
 y  = df_breast[['diagnosis']].values
 
 # Instantiate a DecisionTreeClassifier 'dt' with a maximum depth of 6
-dt = ___
+dt = DecisionTreeClassifier(max_depth=6)
 
 
-X_train, X_test, y_train, y_test =___
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2)
 
 # Fit dt to the training set
-___
+dt.fit(X_train, y_train)
 
 # Predict test set labels
-y_pred =
+y_pred = dt.predict(X_test)
 
 #print predicted labels
-__
+print(y_pred)
 
 # Compute test set accuracy
-acc = ___
+acc = accuracy_score(y_pred, y_pred)
 #print the score
 print("Test set accuracy: {:.2f}".format(acc))
