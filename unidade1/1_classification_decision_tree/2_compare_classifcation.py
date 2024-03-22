15db60e5fa8f3366508ed5fcb03c0648e6b21d0d
DSsavioluis <luissavio150@gmail.com>
2024-03-22 16:48:07 -0300

    att

--- unidade1/1_classification_decision_tree/2_compare_classifcation.py
+++ unidade1/1_classification_decision_tree/2_compare_classifcation.py
@@ -1,4 +1,4 @@
-# Import LogisticRegression from sklearn.linear_model
+#import LogisticRegression from sklearn.linear_model
 from src.utils import load_breast_cancer_dataset
 from sklearn.linear_model import  LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score
 
 
 def process_classifier(clf,X_train, X_test, y_train, y_test):
-    ---
+    train_test_split(X,y, X_train,y_train,X_test,y_test)
 
 
 
@@ -16,13 +16,14 @@ X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean","smooth
 y  = df_breast[['diagnosis']].values
 
 # Instatiate logreg and decision tree
-logreg = ----
-dt = --
+logreg = LogisticRegression()
+dt = DecisionTreeClassifier()
 
 # split the data
-X_train, X_test, y_train, y_test = ---
-# Fit logreg to the training set
+X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.2,random_state=3)
 
-# call function to process log_reg
---
+# Fit logreg to the training set
+fit = dt.fit(X_train,y_train)
+
 # call function to process dt
+
