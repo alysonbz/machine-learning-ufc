d7dccb079d11eadf76096fe50f7adb1a74c6d5fb
Erick Coutinho <erickcoutinho@alu.ufc.br>
2024-03-22 16:50:10 -0300

    2° commit

--- unidade1/1_classification_decision_tree/2_compare_classifcation.py
+++ unidade1/1_classification_decision_tree/2_compare_classifcation.py
@@ -7,8 +7,10 @@ from sklearn.metrics import accuracy_score
 
 
 def process_classifier(clf,X_train, X_test, y_train, y_test):
-    ---
-
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f'A acuracia é{acc}')
 
 
 df_breast = load_breast_cancer_dataset()
@@ -16,13 +18,13 @@ X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean","smooth
 y  = df_breast[['diagnosis']].values
 
 # Instatiate logreg and decision tree
-logreg = ----
-dt = --
+logreg = LogisticRegression()
+dt = DecisionTreeClassifier()
 
 # split the data
-X_train, X_test, y_train, y_test = ---
-# Fit logreg to the training set
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=45,test_size=0.2)
 
 # call function to process log_reg
---
+process_classifier(logreg)
 # call function to process dt
+process_classifier(dt)
--- unidade1/1_classification_decision_tree/5_regression_decision_tree.py
+++ unidade1/1_classification_decision_tree/5_regression_decision_tree.py
@@ -2,7 +2,7 @@ from src.utils import load_auto_dataset
 from sklearn.model_selection import train_test_split
 
 # Import DecisionTreeRegressor from sklearn.tree
-from ___ import ___
+from sklearn.tree import DecisionTreeRegressor
 
 # Import mean_squared_error from sklearn.metrics as MSE
 from sklearn.metrics import mean_squared_error as MSE
@@ -13,21 +13,23 @@ y = df['mpg'].values
 
 
 # Instantiate dt
-dt = ___
+dt = DecisionTreeRegressor(random_state=45,
+                           max_depth=5,
+                           min_samples_leaf=0.1)
 
-X_train, X_test, y_train, y_test = ___
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=45, test_size=0.3)
 
 # Fit dt to the training set
-____
+dt.fit(X_train, y_train)
 
 # Compute y_pred
-y_pred = ___
+y_pred = dt.predict(X_test)
 
 # Compute mse_dt
-mse_dt =___
+mse_dt = MSE(y_test, y_pred)
 
 # Compute rmse_dt
-rmse_dt = ___
+rmse_dt = mse_dt*(1/2)
 
 # Print rmse_dt
 print("Test set RMSE of dt: {:.2f}".format(rmse_dt))
