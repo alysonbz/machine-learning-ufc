44dd3af0d614480e0b5ebc2c1c83ff27c44e010f
Luciana <lucianasmartins779@gmail.com>
2024-03-23 17:43:12 -0300

    aula 22.03

--- unidade1/1_classification_decision_tree/2_compare_classifcation.py
+++ unidade1/1_classification_decision_tree/2_compare_classifcation.py
@@ -6,23 +6,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 
-def process_classifier(clf,X_train, X_test, y_train, y_test):
-    ---
-
-
+def process_classifier(clf, X_train, X_test, y_train, y_test):
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f'Acurácia: {acc}')
 
 df_breast = load_breast_cancer_dataset()
-X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean","smoothness_mean"]].values
-y  = df_breast[['diagnosis']].values
+X = df_breast[["radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean"]].values
+y = df_breast[['diagnosis']].values
 
 # Instatiate logreg and decision tree
-logreg = ----
-dt = --
+log_reg = LogisticRegression()
+dt = DecisionTreeClassifier()
 
 # split the data
-X_train, X_test, y_train, y_test = ---
-# Fit logreg to the training set
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    stratify=y,
+                                                    random_state=1)
 
 # call function to process log_reg
---
+process_classifier(log_reg)
 # call function to process dt
+process_classifier(dt)
--- unidade1/1_classification_decision_tree/3_decision_tree_entropy.py
+++ unidade1/1_classification_decision_tree/3_decision_tree_entropy.py
@@ -2,36 +2,37 @@
 from sklearn.tree import DecisionTreeClassifier
 from src.utils import load_breast_cancer_dataset
 from sklearn.model_selection import train_test_split
+
 # Import accuracy_score from sklearn.metrics
 from sklearn.metrics import accuracy_score
 
 df_breast = load_breast_cancer_dataset()
-X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean","smoothness_mean"]].values
-y  = df_breast[['diagnosis']].values
+X = df_breast[["radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean"]].values
+y = df_breast[['diagnosis']].values
 
 # divida o dataset em treino e teste
-X_train, X_test, y_train, y_test =___(__, __, __, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test =train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-# Instancie aas árvores de decisão com 2 possíveis critérios: entropy e gini
-dt_entropy = ___(max_depth=8,criterion=___, random_state=42)
-dt_gini =___(max_depth=8,criterion=__, random_state=42)
+# Instancie as árvores de decisão com 2 possíveis critérios: entropy e gini
+dt_entropy = DecisionTreeClassifier(max_depth=8,criterion= "entropy", random_state=42)
+dt_gini =DecisionTreeClassifier(max_depth=8,criterion= "gini", random_state=42)
 
 # Fit os objetos dt_gini e dt_entropy
-___
-___
+dt_entropy.fit(X_train, y_train)
+dt_gini.fit(X_train, y_train)
 
 
 # Use dt_entropy e dt_gini para realizar predições no conjunto de teste
-y_pred_entropy= __
-y_pred_gini =
+y_pred_entropy= dt_entropy.predict(X_test)
+y_pred_gini = dt_gini.predict(X_test)
 
 # Evaluate accuracy_entropy
-accuracy_entropy = __
+accuracy_entropy = accuracy_score(y_test, y_pred_entropy)
 
-accuracy_gini = __
+accuracy_gini = accuracy_score(y_test, y_pred_gini)
 
 # Print accuracy_entropy
-___
+print(f'Acuráia de Entropia: {accuracy_entropy}')
 
 # Print accuracy_gini
-__
+print(f'Acuráia de Gini: {accuracy_gini}')
