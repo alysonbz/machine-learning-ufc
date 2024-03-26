86e44098cd163ce7b0e6ec318d4e8253efd9292b
Emilymendc <emilymendc@alu.ufc.br>
2024-03-26 12:14:09 -0300

    Oi, minhas atividades em aprendizado de maquina.

--- unidade1/1_classification_decision_tree/2_compare_classifcation.py
+++ unidade1/1_classification_decision_tree/2_compare_classifcation.py
@@ -7,8 +7,10 @@ from sklearn.metrics import accuracy_score
 
 
 def process_classifier(clf,X_train, X_test, y_train, y_test):
-    ---
-
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f'A acuracia é: {acc}')
 
 
 df_breast = load_breast_cancer_dataset()
@@ -16,13 +18,17 @@ X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean","smooth
 y  = df_breast[['diagnosis']].values
 
 # Instatiate logreg and decision tree
-logreg = ----
-dt = --
+logreg = LogisticRegression(random_state=42)
+dt = DecisionTreeClassifier(max_depth=4, min_samples_split=0.1, random_state=3)
+
 
 # split the data
-X_train, X_test, y_train, y_test = ---
-# Fit logreg to the training set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
 # call function to process log_reg
---
+process_classifier(logreg, X_train, X_test, y_train.ravel(), y_test.ravel())
 # call function to process dt
+process_classifier(dt, X_train, X_test, y_train.ravel(), y_test.ravel())
+
+"""y_train.ravel() é usado para garantir que os rótulos de treinamento estejam no formato correto (1D array) 
+antes de passá-los para os classificadores."""
--- unidade1/1_classification_decision_tree/6_linear _regression_vs_decision_tree.py
+++ unidade1/1_classification_decision_tree/6_linear _regression_vs_decision_tree.py
@@ -1,4 +1,3 @@
-
 from src.utils import load_auto_dataset
 from sklearn.model_selection import train_test_split
 # Import DecisionTreeRegressor from sklearn.tree
@@ -11,16 +10,16 @@ from sklearn.metrics import mean_squared_error as MSE
 def compute_regressor_rmse(reg,X_train, X_test,y_train,y_test):
 
     # Fit dt to the training set
-    ____
+    reg.fit(X_train, y_train)
 
     # Compute y_pred
-    y_pred = ___
+    y_pred = reg.predict(X_test)
 
     # Compute mse_dt
-    mse  = ___
+    mse  = MSE(y_test, y_pred)
 
     # Compute rmse_dt
-    rmse = ___
+    rmse = mse**(1/2)
 
     return rmse
 
@@ -29,18 +28,17 @@ X = df.drop(['mpg','origin'],axis=1)
 y = df['mpg'].values
 
 # Instantiate dt and lr
-dt = ___
-lr = ___
+dt = DecisionTreeRegressor()
+lr = LinearRegression()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3)
 
 # call compute_regressor_rmse function for the two regressors
-rmse_dt = ___
-rmse_lr = ___
+rmse_dt = compute_regressor_rmse(dt,X_train, X_test,y_train,y_test )
+rmse_lr = compute_regressor_rmse(lr,X_train, X_test,y_train,y_test)
 
 # Print rmse_lr
 print('Linear Regression test set RMSE: {:.2f}'.format(rmse_lr))
 
 # Print rmse_dt
 print('Regression Tree test set RMSE: {:.2f}'.format(rmse_dt))
-
