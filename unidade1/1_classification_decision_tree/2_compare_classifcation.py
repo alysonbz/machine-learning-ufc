ad77aa84c0b16159bae967f07e1083c1da56ae64
livia094 <sousalivia257@gmail.com>
2024-03-27 09:44:39 -0300

    atividades

--- unidade1/1_classification_decision_tree/2_compare_classifcation.py
+++ unidade1/1_classification_decision_tree/2_compare_classifcation.py
@@ -7,22 +7,23 @@ from sklearn.metrics import accuracy_score
 
 
 def process_classifier(clf,X_train, X_test, y_train, y_test):
-    ---
-
-
+    clf.fit(X_train,y_train)
+    y_pred= clf.predict(X_test)
+    accuracy= accuracy_score(y_test, y_pred)
+    print('Accuracy:', accuracy)
 
 df_breast = load_breast_cancer_dataset()
 X = df_breast[["radius_mean","texture_mean","perimeter_mean","area_mean","smoothness_mean"]].values
 y  = df_breast[['diagnosis']].values
 
 # Instatiate logreg and decision tree
-logreg = ----
-dt = --
+logreg = LogisticRegression()
+dt = DecisionTreeClassifier()
 
 # split the data
-X_train, X_test, y_train, y_test = ---
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0)
 # Fit logreg to the training set
+process_classifier(logreg, X_train, X_test, y_train, y_test)
 
-# call function to process log_reg
---
 # call function to process dt
+process_classifier(dt,X_train, X_test, y_train, y_test)
