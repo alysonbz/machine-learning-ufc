355ca2709b8bd72b2e9305cb86a51bbd2dbc1a1b
MASTER <aryaanialekyni@gmail.com>
2024-07-17 18:42:00 -0300

    quase todas as quests

--- unidade2/1-SVM/1_train_svm.py
+++ unidade2/1-SVM/1_train_svm.py
@@ -11,16 +11,16 @@ y = wine['Quality'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Train a linear SVM
-svm = ___
+svm = SVC(gamma=2)
 
 # fit svm classifier
---
+svm.fit(X_train, y_train)
 
 # predict with svm classifier
-y_pred = ---
+y_pred = svm.predict(X_test)
 
 # print classification report matrix
-print (____)
+print (classification_report())
 
 # show confusion matrix
 cm = ---
