c7c9dcc3075a6ebcca874bced840714049c044ec
Luciana <lucianasmartins779@gmail.com>
2024-09-20 17:59:57 -0300

    .

--- unidade2/1-SVM/3_gridsearch_more_features.py
+++ unidade2/1-SVM/3_gridsearch_more_features.py
@@ -9,18 +9,14 @@ y = diabetes_df['diabetes'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
 # Instantiate an RBF SVM
-svm = ___
+rbf = SVC(kernel="rbf")
 
 # Instantiate the GridSearchCV object and run the search
-parameters = {'C':[0.1, 1, 10], 'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
-searcher = ___
+parameters = {'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(param_grid=parameters,estimator=rbf)
 
 #fit the searcher
-___
+searcher.fit(X_train, y_train)
 
-# Report the best parameters and the corresponding score
-print("Best CV params", ___)
-print("Best CV accuracy", ___)
-
-# Report the test accuracy using these best parameters
-print("Test accuracy of best grid search hypers:", searcher.score(X_test,y_test))
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
